ae2ce63f353cf58245706246cde1f32b2348d19b
camilo garcia <camilo456garcia@gmail.com>
2022-09-28 18:11:25 -0500

    staking

--- src/views/acciones/Acciones.vue
+++ src/views/acciones/Acciones.vue
@@ -26,14 +26,14 @@
         </div>
         <h5>Fondeado</h5>
         <article class="acciones__card__text__progress">
-            <b-progress
-              :value="valueProgress"
-              :max="100"
-              animated
-              variant="Primary"
-            ></b-progress>
-            <span>{{ valueProgress }}%</span>
-          </article>
+          <b-progress
+            :value="valueProgress"
+            :max="100"
+            animated
+            variant="Primary"
+          ></b-progress>
+          <span>{{ valueProgress }}%</span>
+        </article>
         <div>
           <p>Recaudo total</p>
           <p>$0.00</p>
@@ -46,48 +46,47 @@
           <p>Mes</p>
           <p>0/6</p>
         </div>
-        <BaseButton label="Invertir ahora" @click="$router.push(({ path: '/acciones/deposit' }))"></BaseButton>
+        <BaseButton
+          label="Invertir ahora"
+          @click="$router.push({ path: '/acciones/deposit' })"
+        ></BaseButton>
       </article>
     </section>
     <article class="acciones__container">
       <section class="acciones__card acciones__move" v-if="!show">
-        <article class="acciones__card__img">
-          <img src="/assets/images/bg-accion.webp" alt="" />
-        </article>
+        <section class="acciones__move__img">
+          <article class="acciones__card__img">
+            <img src="/assets/images/bg-accion.webp" alt="" />
+          </article>
+          <article>
+            <h2>Kodex</h2>
+            <h3>KXP</h3>
+          </article>
+        </section>
+        <section class="acciones__move__data"> 
+          <h5>Periodo de tiempo</h5>
+          <p>6 Meses</p>
+          <h5>Porcentaje mensual</h5>
+          <p>0,3%</p>
+        </section>
         <article class="acciones__card__text">
-          <h2>Kodex</h2>
-          <h3>KXP</h3>
-          <h5>Fondeado</h5>
-          <article class="acciones__card__text__progress">
-              <b-progress
-                :value="valueProgress"
-                :max="100"
-                animated
-                variant="Primary"
-              ></b-progress>
-              <span>{{ valueProgress }}%</span>
-            </article>
+          <h4>Estado del programa</h4>
           <div>
-            <p>Inversión</p>
-            <p>1,000.00 KP</p>
+            <p>Total tokens stakes</p>
+            <p>0.00</p>
           </div>
           <div>
-            <p>Porcentaje mensual</p>
-            <p>0,3%</p>
+            <p>Stake total</p>
+            <p>0.00</p>
           </div>
           <div>
-            <p>Fecha de retiros</p>
-            <p>05 de cada mes</p>
+            <p>Minimo stake</p>
+            <p>0.00</p>
           </div>
-          <div>
-            <p>Retorno de inversión</p>
-            <p>$0.00</p>
-          </div>
-          <div>
-            <p>Mes</p>
-            <p>0/6</p>
-          </div>
-          <BaseButton label="Retirar" @click="$router.push(({ path: '/acciones/withdraw' }))"></BaseButton>
+          <BaseButton
+            label="Retirar"
+            @click="$router.push({ path: '/acciones/withdraw' })"
+          ></BaseButton>
         </article>
       </section>
     </article>
@@ -96,18 +95,18 @@
 
 <script>
 import Header from "@/components/Header.vue";
-import { ref } from '@vue/reactivity';
+import { ref } from "@vue/reactivity";
 import BaseButton from "@/components/form/BaseButton.vue";
 import ToggleButton from "@/components/base/ToggleButton.vue";
 
 export default {
   components: { Header, BaseButton, ToggleButton },
   setup() {
-    const show = ref(true)
+    const show = ref(true);
     const valueProgress = ref(45);
     return {
       valueProgress,
-      show
+      show,
     };
   },
 };
@@ -119,11 +118,14 @@ export default {
   padding: 40px;
   border-radius: 8px;
   background: white;
+  @media (max-width: 1000px){
+    padding: 40px 20px;
+  }
   &__card {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 20px;
-    @media (max-width:1000px) {
+    @media (max-width: 1000px) {
       grid-template-columns: 1fr;
     }
     &__img {
@@ -137,7 +139,7 @@ export default {
     &__text {
       h2 {
       }
-      button{
+      button {
         margin-bottom: 0;
       }
       h3 {
@@ -162,27 +164,61 @@ export default {
         margin-bottom: 10px;
       }
       &__progress {
-          display: grid;
-          align-items: center;
-          grid-template-columns: 1fr 30px;
-          grid-gap: 20px;
-          margin-top: 20px;
-          font-size: 14px;
-          font-weight: 500;
-        }
+        display: grid;
+        align-items: center;
+        grid-template-columns: 1fr 30px;
+        grid-gap: 20px;
+        margin-top: 20px;
+        font-size: 14px;
+        font-weight: 500;
+      }
     }
   }
-  &__container{
+  &__container {
     display: flex;
     justify-content: space-between;
   }
-  &__move{
+  &__move {
     grid-template-columns: 1fr;
-    max-width: 350px;
+    width: 350px;
     margin-right: 20px;
     margin-bottom: 20px;
+    padding: 25px;
+    border-radius: 16px;
+    background: #F6F8FA;
+    h4{
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+    &__img {
+      display: flex;
+      img {
+        width: 80px;
+        object-fit: cover;
+        margin-right: 20px;
+      }
+      h2,
+      h3 {
+        font-size: 20px;
+      }
+      h3 {
+        font-weight: 300;
+      }
+    }
+    &__data{
+      border-bottom: 1px solid #EBEBEB;
+      padding-bottom: 10px;
+      h5{
+        font-weight: 300;
+        font-size: 14px;
+        margin-bottom: 5px;
+      }
+      p{
+        color: #132D7C;
+      }
+    }
   }
-  .toggleButton{
+  .toggleButton {
     max-width: 300px;
     margin: 20px auto;
   }
